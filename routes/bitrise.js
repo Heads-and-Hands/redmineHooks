fa6aeb5a2c62adf775ff938690d7bc082a0362ba
cxolast <cxolast@mail.ru>
2018-03-26 13:20:59 +0300

    bitrise change log

--- routes/bitrise.js
+++ routes/bitrise.js
@@ -6,18 +6,18 @@ var redmineService = require('./../modules/redmine')
 const key = require('./../key.js').key
 var redmine = new redmineService.Redmine(key.redmine)
 
-router.post('/', function(req, res, next) {
+router.post('/', function (req, res, next) {
   let q = qs.parse(req.url.split('?')[1])
   let reqParsed = req.body
-  fs.appendFile('./log.txt',
-    new Date() + "\r\n" + req.url + ' ' + JSON.stringify(reqParsed) + "\r\n\n", function (err) {}
-  );
+  let log = new Date() + "\r\n"
+  fs.appendFile('./log-bitrise.txt', new Date() + "\r\n" + req.url + ' ' + JSON.stringify(reqParsed) + "\r\n\n", () => {});
   if (reqParsed.build_status === 1 && reqParsed.git.src_branch === 'develop' && reqParsed.git.dst_branch === 'develop') {
-    console.log('bitrise build ' + q.project + ' ' +  reqParsed.build_number)
+    log += new Date() + 'bitrise build ' + q.project + ' ' + reqParsed.build_number
     redmine.bitriseHook(q.project, reqParsed.build_number)
   } else {
-    console.log('bitrise not now')
+    log += 'bitrise not now'
   }
+  fs.appendFile('./log-bitrise.txt', log + "\r\n\n", () => {});
   res.send('bitrise end');
 });
 
