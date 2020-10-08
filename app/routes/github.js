ae25a49dc71f70f5890dddf0a8c583ac3917a145
Anton Maksimov <anton.maximov@gmail.com>
2020-10-08 14:50:51 +0300

    Project name from repositiorest field

--- app/routes/github.js
+++ app/routes/github.js
@@ -93,7 +93,7 @@ router.post('/', async function (req, res, next) {
     date: new Date(),
     author: author,
     tasks: taskNumbers.join(),
-    project: payload.pull_request ? payload.pull_request.head.repo.name : '',
+    project: payload.repository ? payload.repository.name : '',
     event: event,
     action: action,
     needAssign: needAssign
